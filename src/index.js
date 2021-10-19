5fb59a54196b9b09d2d332b07b37e07e62fcdb53
Bárbara Mota Santana <barbaramotabms@gmail.com>
2021-10-19 20:33:12 -0300

    testando cors

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, *");
     next();
-});
+});*/
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
