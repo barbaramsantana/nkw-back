99cfa476bcb30de24ed544d7d832829c5b759925
Bárbara Mota Santana <barbaramotabms@gmail.com>
2021-10-19 20:08:24 -0300

    api cors

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, *");
-    //app.use(cors());
     next();
 });
 
